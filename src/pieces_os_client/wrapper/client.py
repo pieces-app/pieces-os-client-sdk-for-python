49a8923eec183f2b34eea26c4aa1e5f71e26cf9c
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-01 07:50:14 +0300

    feat open_pieces_os

--- src/pieces_os_client/wrapper/client.py
+++ src/pieces_os_client/wrapper/client.py
@@ -25,6 +25,7 @@ from pieces_os_client import (
 from typing import Optional,Dict
 import platform
 import atexit
+import subprocess
 
 
 from .copilot import Copilot
@@ -60,8 +61,8 @@ class PiecesClient:
         self.linkfy_api = LinkifyApi(self.api_client)
 
         # Websocket urls
-        if not self.host.startswith("http"):
-            raise TypeError("Invalid host url\n Host should start with http or https")
+        if self.host.startswith("http"):
+            raise ValueError("Invalid host url\n Host should start with http or https")
         ws_base_url:str = self.host.replace('http','ws')
         
         self.ASSETS_IDENTIFIERS_WS_URL = ws_base_url + "/assets/stream/identifiers"
@@ -70,12 +71,12 @@ class PiecesClient:
         self.CONVERSATION_WS_URL = ws_base_url + "/conversations/stream/identifiers"
         self.HEALTH_WS_URL = ws_base_url + "/.well-known/stream/health"
 
-        local_os = platform.system().upper() if platform.system().upper() in ["WINDOWS","LINUX","DARWIN"] else "WEB"
-        local_os = "MACOS" if local_os == "DARWIN" else local_os
+        self.local_os = platform.system().upper() if platform.system().upper() in ["WINDOWS","LINUX","DARWIN"] else "WEB"
+        self.local_os = "MACOS" if self.local_os == "DARWIN" else self.local_os
         seeded_connector = seeded_connector or SeededConnectorConnection(
             application=SeededTrackedApplication(
                 name = "OPEN_SOURCE",
-                platform = local_os,
+                platform = self.local_os,
                 version = __version__)) 
 
         self.tracked_application = self.connector_api.connect(seeded_connector_connection=seeded_connector).application
@@ -151,11 +152,29 @@ class PiecesClient:
         return self.well_known_api.get_well_known_version()
     
     @property
-    def health(self) -> str:
+    def health(self) -> bool:
         """
-            Returns Pieces OS health
+            Returns True Pieces OS health is ok else False
         """
-        return self.well_known_api.get_well_known_health()
+        try:
+            return self.well_known_api.get_well_known_health().startswith("ok")
+        except:
+            pass
+        return False
+
+    def open_pieces_os(self) -> bool:
+        """
+            Open Pieces OS
+
+            Returns (bool): true if Pieces OS runned successfully else false 
+        """
+        if self.local_os == "WINDOWS":
+            subprocess.run(["start", "pieces://launch"], shell=True)
+        elif self.local_os == "MACOS":
+            subprocess.run(["open","pieces://launch"])
+        elif self.local_os == "LINUX":
+            subprocess.run(["xdg-open","pieces://launch"])
+        return self.health
 
 # Register the function to be called on exit
 atexit.register(BaseWebsocket.close_all)
