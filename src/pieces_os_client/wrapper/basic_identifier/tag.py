7c34ad7b8418a04685be9918107bbda537887efb
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-19 20:00:36 +0300

    add doc string to BasicTag

--- src/pieces_os_client/wrapper/basic_identifier/tag.py
+++ src/pieces_os_client/wrapper/basic_identifier/tag.py
@@ -11,7 +11,25 @@ if TYPE_CHECKING:
 	from ..client import PiecesClient
 
 class BasicTag(Basic):
-	def __init__(self,pieces_client:"PiecesClient", id:str) -> None:
+	"""
+	Represents a basic tag in the system.
+
+	Attributes:
+	- pieces_client (PiecesClient): The client used to interact with the Pieces API.
+	- tag (Tag): The tag object associated with this BasicTag instance.
+	"""
+
+	def __init__(self, pieces_client: "PiecesClient", id: str) -> None:
+		"""
+		Initializes a BasicTag instance.
+
+		Args:
+		- pieces_client (PiecesClient): The client used to interact with the Pieces API.
+		- id (str): The ID of the tag.
+
+		Raises:
+		- ValueError: If there is an error retrieving the tag.
+		"""
 		try:
 			self.tag = pieces_client.tag_api.tags_specific_tag_snapshot(
 				id, transferables=True)
@@ -19,9 +37,19 @@ class BasicTag(Basic):
 			raise ValueError("Error in retrieving the tag")
 		self.pieces_client = pieces_client
 		super().__init__(id)
-
+	
 	@classmethod
-	def tag_from_raw_content(cls,pieces_client:"PiecesClient",raw_content:str) -> "BasicTag":
+	def tag_from_raw_content(cls, pieces_client: "PiecesClient", raw_content: str) -> "BasicTag":
+		"""
+		Creates a BasicTag instance based on raw content.
+
+		Args:
+		- pieces_client (PiecesClient): The client used to interact with the Pieces API.
+		- raw_content (str): The raw content of the tag.
+
+		Returns:
+		- BasicTag: The created BasicTag instance.
+		"""
 		existance = pieces_client.tags_api.tags_exists(ExistentMetadata(
 			value=raw_content
 		))
@@ -34,41 +62,97 @@ class BasicTag(Basic):
 			)
 
 	@staticmethod
-	def create_tag(pieces_client:"PiecesClient",seeded_tag:SeededTag) -> "BasicTag":
+	def create_tag(pieces_client: "PiecesClient", seeded_tag: SeededTag) -> "BasicTag":
+		"""
+		Creates a new tag based on a seeded tag.
+
+		Args:
+		- pieces_client (PiecesClient): The client used to interact with the Pieces API.
+		- seeded_tag (SeededTag): The seeded tag object.
+
+		Returns:
+		- BasicTag: The created BasicTag instance.
+		"""
+
 		return BasicTag(pieces_client,
 			pieces_client.tags_api.tags_create_new_tag(
 				transferables=False,
 				seeded_tag=seeded_tag).id
 			)
-	
+
 	@property
 	def id(self) -> str:
+		"""
+		Gets the ID of the tag.
+
+		Returns:
+		- str: The ID of the tag.
+		"""
 		return self.tag.id
 
 	@property
 	def raw_content(self) -> str:
+		"""
+		Gets the raw content of the tag.
+
+		Returns:
+		- str: The raw content of the tag.
+		"""
 		return self.tag.text
 
 	@raw_content.setter
-	def raw_content(self,raw_content):
+	def raw_content(self, raw_content):
+		"""
+		Sets the raw content of the tag.
+
+		Args:
+		- raw_content: The raw content to be set.
+		"""
 		self.tag.text = raw_content
 		self._edit_tag(self.tag)
 
 	@property
 	def assets(self) -> Optional[List["BasicAsset"]]:
+		"""
+		Gets the assets associated with the tag.
+
+		Returns:
+		- Optional[List["BasicAsset"]]: A list of BasicAsset objects associated with the tag.
+		"""
 		from . import BasicAsset
 		if self.tag.assets and self.tag.assets.iterable:
 			return [BasicAsset(asset.id) for asset in self.tag.assets.iterable]
 
-	def associate_asset(self,asset:"BasicAsset"):
+	def associate_asset(self, asset: "BasicAsset"):
+		"""
+		Associates an asset with the tag.
+
+		Args:
+		- asset (BasicAsset): The asset to be associated.
+		"""
 		self.pieces_client.tag_api.tag_associate_asset(asset.id,self.tag.id)
 
-	def disassociate_asset(self,asset:"BasicAsset"):
+	def disassociate_asset(self, asset: "BasicAsset"):
+		"""
+		Disassociates an asset from the tag.
+
+		Args:
+		- asset (BasicAsset): The asset to be disassociated.
+		"""
 		self.pieces_client.tag_api.tag_disassociate_asset(self.tag.id,asset.id)
 
 	def delete(self):
+		"""
+		Deletes the tag.
+		"""
 		self.pieces_client.tags_api.tags_delete_specific_tag(self.tag.id)
 
-	def _edit_tag(self,tag:Tag):
+	def _edit_tag(self, tag: Tag):
+		"""
+		Edits the tag.
+
+		Args:
+		- tag (Tag): The tag object to be edited.
+		"""
 		self.pieces_client.tag_api.tag_update(False,tag)
 
