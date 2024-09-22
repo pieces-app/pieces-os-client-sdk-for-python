c8812a87ffc52ceb38b15633f18cfa170ae45c59
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-09-22 22:52:39 +0530

    Create test_basic_tag.py
    - Load basic dependencies .

---
+++ tests/test_basic_tag.py
@@ -0,0 +1,4 @@
+import unittest
+from unittest.mock import Mock, patch, MagicMock
+from pieces_os_client.models import Tag, SeededTag, ExistentMetadata
+from pieces_os_client.wrapper.basic_identifier.tag import BasicTag
