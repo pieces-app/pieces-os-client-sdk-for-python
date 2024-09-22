298f5121078394fa131dfff107fed30c805b462f
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-09-22 22:22:40 +0530

    Basic imports
    Setting up basic imports from scratch.

---
+++ tests/test_basic_website.py
@@ -0,0 +1,4 @@
+import unittest
+from unittest.mock import Mock, patch, PropertyMock
+from pieces_os_client.models import Website, SeededWebsite, ExistentMetadata
+from pieces_os_client.wrapper.basic_identifier.website import BasicWebsite
