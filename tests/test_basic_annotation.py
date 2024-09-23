744d0d248a1f18a9c50ceae258f1885737db3010
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-09-23 16:02:44 +0530

    Add TestBasicAnnotation class for unit testing
    - Creates a mock PiecesClient
    - Creates a mock Annotation with specified attributes
    - Initializes a BasicAnnotation instance with the mocks

--- tests/test_basic_annotation.py
+++ tests/test_basic_annotation.py
@@ -4,3 +4,12 @@ from pieces_os_client.models.annotation import Annotation
 from pieces_os_client.models.annotation_type_enum import AnnotationTypeEnum
 from pieces_os_client.models.seeded_annotation import SeededAnnotation
 from pieces_os_client.wrapper.basic_identifier.annotation import BasicAnnotation
+
+class TestBasicAnnotation(unittest.TestCase):
+    def setUp(self):
+        self.mock_pieces_client = Mock()
+        self.mock_annotation = Mock(spec=Annotation)
+        self.mock_annotation.id = "test_annotation_id"
+        self.mock_annotation.type = AnnotationTypeEnum.COMMENT
+        self.mock_annotation.text = "Test annotation text"
+        self.basic_annotation = BasicAnnotation(self.mock_pieces_client, self.mock_annotation)
