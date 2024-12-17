a202c994541991591bc1df0d2d16e1116483d6d2
bishoy-at-pieces <bishoy@pieces.app>
2024-12-17 19:02:35 +0200

    add dynamic ports

--- src/pieces_os_client/wrapper/websockets/health_ws.py
+++ src/pieces_os_client/wrapper/websockets/health_ws.py
@@ -7,3 +7,10 @@ class HealthWS(BaseWebsocket):
 
 	def on_message(self, ws, message):
 		self.on_message_callback(message)
+		if message == "OK":
+			self.pieces_client.is_pos_stream_running = True
+	
+	def on_close(self, ws, close_status_code, close_msg):
+		self.pieces_client.is_pos_stream_running = False
+		self.pieces_client.port = None # Reset the port
+		super().on_close(ws, close_status_code, close_msg)
